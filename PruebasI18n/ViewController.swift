f43e1216796c0dab5c23a6f6f4e4101119594d93
cice <cice@aula1povm22-10.local>
2018-01-30 19:09:30 +0100

    Initial Commit

---
+++ PruebasI18n/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PruebasI18n
+//
+//  Created by cice on 30/1/18.
+//  Copyright © 2018 TATINC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
