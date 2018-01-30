ad3355ea9021a1d0849ce071b77ef8483b9117b7
cice <cice@aula1povm22-10.local>
2018-01-30 20:31:19 +0100

    complete

--- PruebasI18n/ViewController.swift
+++ PruebasI18n/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.navigationItem.title = NSLocalizedString("TITULO_ESCENA_INICIAL", comment: "Hola traductor")
     }
 
     override func didReceiveMemoryWarning() {
